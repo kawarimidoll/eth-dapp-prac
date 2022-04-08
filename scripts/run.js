baa548110627784db6ff3ca49cbbb88fecbad9f2
kawarimidoll <kawarimidoll+git@gmail.com>
2022-04-08 19:40:46 +0900

    feat: read and write contract

--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,27 @@
 const main = async () => {
+  const [owner, signer1, signer2] = await hre.ethers.getSigners();
+
   // We get the contract to deploy
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   const wavePortal = await waveContract.deployed();
 
   console.log("WavePortal deployed to:", wavePortal.address);
+  console.log("WavePortal deployed by:", owner.address);
+
+  await waveContract.getTotalWaves();
+
+  const waveByOwner = await waveContract.wave();
+  await waveByOwner.wait();
+  await waveContract.getTotalWaves();
+
+  const waveBySigner1 = await waveContract.connect(signer1).wave();
+  await waveBySigner1.wait();
+  await waveContract.getTotalWaves();
+
+  const waveBySigner2 = await waveContract.connect(signer2).wave();
+  await waveBySigner2.wait();
+  await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
